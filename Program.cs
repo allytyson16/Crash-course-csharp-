47dff1453c7e94b7e0da7734b50c0298b56db465
allytyson16 <adelaideyeboah2@gmail.com>
2022-03-11 23:15:53 +0000

    third update

--- Program.cs
+++ Program.cs
@@ -6,39 +6,76 @@ namespace csharp_outube_crash_course
     {
         static void Main(string[] args)
         {
-            Console.Write("Hey!");
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("I love candies!");
-            Console.WriteLine("I am a Christian!!");
-            Console.WriteLine("And I am not afraid of the Gospel!");
-            Console.Write("I am a gamer");
-            Console.Beep();     //for a beep after compiling
+            //Console.Write("Hey!");
+            //Console.WriteLine("Hello World!");
+            //Console.WriteLine("I love candies!");
+            //Console.WriteLine("I am a Christian!!");
+            //Console.WriteLine("And I am not afraid of the Gospel!");
+            //Console.Write("I am a gamer");
+            //Console.Beep();     //for a beep after compiling
 
-            //the use of escape characters.
-            Console.WriteLine("Hey!");
-            Console.WriteLine("\t You ");
-            Console.WriteLine(" \t\tare");
-            Console.WriteLine(" \t\t\ta");
-            Console.WriteLine(" \t\t\t\twitch");
+
+            ////the use of escape characters.
+            //Console.WriteLine("Hey!");
+            //Console.WriteLine("\t You ");
+            //Console.WriteLine(" \t\tare");
+            //Console.WriteLine(" \t\t\ta");
+            //Console.WriteLine(" \t\t\t\twitch");
+
 
             //data types
-            int age = 21;
-            double height = 299.2;
-            bool alive = true;
-            char symbol = '@';
-            string name = "Mina";
+            //int age = 21;
+            //double height = 299.2;
+            //bool alive = true;
+            //char symbol = '@';
+            //string name = "Mina";
 
-            Console.WriteLine("Hello " + name);
-            Console.WriteLine("You are " + age + "years");
-            Console.WriteLine("Your height is " + height);
-            Console.WriteLine("You are alive!" + alive);
-            Console.WriteLine("Your symbol is " + symbol);
-            Console.WriteLine("Your username is " + symbol + name);
+            //Console.WriteLine("Hello " + name);
+            //Console.WriteLine("You are " + age + "years");
+            //Console.WriteLine("Your height is " + height);
+            //Console.WriteLine("You are alive!" + alive);
+            //Console.WriteLine("Your symbol is " + symbol);
+            //Console.WriteLine("Your username is " + symbol + name);
+
 
             //const
-            const double PI = 3.142;
-            Console.WriteLine(PI);
+            //const double PI = 3.142;
+            //Console.WriteLine(PI);
 
+            //type casting
+            //double to int
+            double a = 34.34;
+            int b = Convert.ToInt32(a);
+            Console.WriteLine(b);
+            //to get data type
+            Console.WriteLine(b.GetType());
+
+            //int to double
+            int w = 344;
+            double y = Convert.ToDouble(w) + 0.3;
+            Console.WriteLine(y);
+            //to get data type
+            Console.WriteLine(y.GetType());
+
+            //int to string
+            int c = 24;
+            string word = Convert.ToString(c);
+            Console.WriteLine(word);
+            //to get data type
+            Console.WriteLine(word.GetType());
+
+            //string to char
+            string alph = "&";
+            char t = Convert.ToChar(alph);
+            //for data type
+            Console.WriteLine(t.GetType());
+
+            //string to boolean
+            string state = "true";
+            bool ans = Convert.ToBoolean(state);
+            Console.WriteLine(ans);
+            //for data type
+            Console.WriteLine(ans.GetType());
 
         }
     }
