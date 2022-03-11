4df18922ea3291e473b6fcedd0affc254d8c6055
allytyson16 <adelaideyeboah2@gmail.com>
2022-03-11 22:16:52 +0000

    first post

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace csharp_outube_crash_course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Write("Hey!");
+            //Console.WriteLine("Hello World!");
+            //Console.WriteLine("I love candies!");
+            //Console.WriteLine("I am a Christian!!");
+            //Console.WriteLine("And I am not afraid of the Gospel!");
+            //Console.Write("I am a gamer");
+            //Console.Beep();
+
+            //Console.WriteLine("Hey!");
+            //Console.WriteLine("\t You ");
+            //Console.WriteLine(" \t\tare");
+            //Console.WriteLine(" \t\t\ta");
+            //Console.WriteLine(" \t\t\t\twitch");
+
+
+            int age = 21;
+            double height = 299.2;
+            bool alive = true;
+            char symbol = '@';
+            string name = "Mina";
+
+            Console.WriteLine("Hello " + name);
+            Console.WriteLine("You are " + age + "years");
+            Console.WriteLine("Your height is " + height);
+            Console.WriteLine("You are alive!" + alive);
+            Console.WriteLine("Your symbol is " + symbol);
+            Console.WriteLine("Your username is " + symbol + name);
+
+        }
+    }
+}
