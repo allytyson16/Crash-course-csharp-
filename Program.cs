07db3c2e1bc22af5b1038000950bba19f7de3940
allytyson16 <adelaideyeboah2@gmail.com>
2022-03-16 10:16:32 +0000

    Guessing Game

--- Program.cs
+++ Program.cs
@@ -364,6 +364,59 @@ namespace csharp_outube_crash_course
                 Console.WriteLine();
             }
 
+
+            //Guessing game
+            Random random2 = new Random();
+            bool playAgain = true;
+            int min = 1;
+            int max = 100;
+            int guess;
+            int guesses;
+            int number;
+            string response;
+
+            while (playAgain)
+            {
+                guess = 0;
+                guesses = 0;
+                response = "";
+                number = random.Next(min, max + 1);
+
+                while(guess != number)
+                {
+                    Console.WriteLine("Guess a number between " + min + " - " + max + " : ");
+                    guess = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Guess: " + guess);
+
+                    if(guess > number)
+                    {
+                        Console.WriteLine(guess + " is too high.");
+                    }
+                    else if (guess < number)
+                    {
+                        Console.WriteLine(guess + " is too low.");
+                    }
+                    guesses++;
+                }
+                Console.WriteLine("Number: " + number);
+                Console.WriteLine("YOU WIN!!");
+                Console.WriteLine("Guesses: " + guesses);
+
+                Console.WriteLine("Would you like to play again (Y/N)");
+                response = Console.ReadLine();
+                response = response.ToUpper();
+
+                if (response == "Y")
+                {
+                    playAgain = true;
+                }
+                else
+                {
+                    playAgain = false;
+                }
+            }
+            Console.WriteLine("Thanks for playing....Try tougher next time.");
+
         }
     }
 }
